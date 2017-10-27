64f6991ba8ff511695ab39a3f1c575e7ed023662
Clark Murray <clarkamurray@gmail.com>
2017-10-27 16:07:22 -0400

    .env file

--- database.php
+++ database.php
@@ -1,25 +1,29 @@
 <?php function getDb() {
 
-			$raw_url = 'postgres://ibxqbxmakaeptv:cb99fc2b41e6e9ab6cb6b6514f7cc21ac7b186b579d4a5c3f7ca9cf0fd42cd6a@ec2-184-73-159-137.compute-1.amazonaws.com:5432/d652anc14vc3r';
+		if (file_exists('.env')) {
+			require __DIR__ . '/vendor/autoload.php';
+			$dotenv = new Dotenv\Dotenv(__DIR__);
+			$dotenv-> load();
+		};
+		
+		$url = parse_url(getenv("DATABASE_URL"));
 
-			$url = parse_url($raw_url);
+		$db_port = $url['port'];
+		$db_host = $url['host'];
+		$db_user = $url['user'];
+		$db_pass = $url['pass'];
+		$db_name = substr($url['path'], 1);
 
-			$db_port = $url['port'];
-			$db_host = $url['host'];
-			$db_user = $url['user'];
-			$db_pass = $url['pass'];
-			$db_name = substr($url['path'], 1);
+		$db = pg_connect(
+		"host=" . $db_host .
+		" port=" . $db_port .
+		" dbname=" . $db_name .
+		" user=" . $db_user .
+		" password=" . $db_pass
+		);
 
-			$db = pg_connect(
-			"host=" . $db_host .
-			" port=" . $db_port .
-			" dbname=" . $db_name .
-			" user=" . $db_user .
-			" password=" . $db_pass
-			);
+		return $db;
 
-			return $db;
-
-		}
+	}
 
 ?>
