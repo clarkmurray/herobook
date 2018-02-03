546c2fdc9f7e7cb14a77309b166ddebee992da28
Clark Murray <clarkamurray@gmail.com>
2018-02-03 11:40:46 -0500

    Cleaned up header.php

--- header.php
+++ header.php
@@ -1,27 +1,13 @@
-<?php
-
-
-	echo "<header>";
-
-	echo "<div class='container-fluid'>";
-
-	echo "<div class='row align-items-center'>";
-
-	echo "<div class='col logoCol'>";
-		echo "<a href='index.php'><img src='./assets/logo.png' /></a>";
-	echo "</div>";
-
-	echo "<div class='col siteName'>";
-		echo "<a href='index.php'><h1 class='text-center'>BAT-NET</h1></a>";
-	echo "</div>";
-
-	echo "<div class='col'>";
-	echo "</div>";
-
-	echo "</div>";
-
-	echo "</div>";
-
-	echo "</header>";
-
-?>
+<header>
+	<div class="container-fluid">
+		<div class="row align-items-center">
+			<div class="col logoCol">
+				<a href="index.php"><img src="./assets/logo.png" /></a>
+			</div>
+			<div class="col siteName">
+				<a href="index.php"><h1 class="text-center">Bat-Net</h1></a>
+			</div>
+			<div class="col"></div>
+		</div>
+	</div>
+</header>
